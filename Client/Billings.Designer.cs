b6735e3ea5d4a21b4b4b1a8462e00878b25b3da3
SK ASHRAFUL ALI APU <23-54917-3@student.aiub.edu>
2026-01-19 22:44:34 +0600

    changed code a little on the biling section

--- Client/Billings.Designer.cs
+++ Client/Billings.Designer.cs
@@ -44,14 +44,14 @@
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Location = new System.Drawing.Point(12, 12);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(342, 426);
+            this.dataGridView1.Size = new System.Drawing.Size(446, 426);
             this.dataGridView1.TabIndex = 0;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(552, 27);
+            this.label1.Location = new System.Drawing.Point(653, 49);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(60, 16);
             this.label1.TabIndex = 1;
@@ -60,16 +60,16 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(454, 114);
+            this.label2.Location = new System.Drawing.Point(555, 120);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(59, 13);
+            this.label2.Size = new System.Drawing.Size(51, 13);
             this.label2.TabIndex = 2;
-            this.label2.Text = "Order No. :";
+            this.label2.Text = "Client Id :";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(454, 197);
+            this.label3.Location = new System.Drawing.Point(555, 203);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(81, 13);
             this.label3.TabIndex = 3;
@@ -77,7 +77,7 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(555, 106);
+            this.textBox1.Location = new System.Drawing.Point(656, 112);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 20);
             this.textBox1.TabIndex = 4;
@@ -94,19 +94,20 @@
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(555, 194);
+            this.textBox2.Location = new System.Drawing.Point(656, 200);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(100, 20);
             this.textBox2.TabIndex = 5;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(555, 274);
+            this.button1.Location = new System.Drawing.Point(656, 280);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(93, 36);
             this.button1.TabIndex = 6;
             this.button1.Text = "Proceed";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Billings
             // 
